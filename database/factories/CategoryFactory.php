ee38d19cbb9e1eeef4837198c4c5c853cbc0f153
AnMashinyan <99428492+AnMashinyan@users.noreply.github.com>
2022-12-15 02:32:32 +0400

    Update CategoryFactory.php

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,28 +1,27 @@
-<?php
-//
-//namespace Database\Factories;
-//
-//use App\Models\Category;
-//use Illuminate\Database\Eloquent\Factories\Factory;
-//use Illuminate\Support\Facades\DB;
-//
-//class CategoryFactory extends Factory
-//{
-//    /**
-//     * Define the model's default state.
-//     *
-//     * @return array
-//     */
-//    public function definition()
-//    {
-//        Category::truncate();
-//        $cat = [
-//            ['title' => 'Մանկավարժական խորհուրդ'],
-//            ['title' => 'Կառավարման խորհուրդ'],
-//            ['title' => 'Ծնողական խորհուրդ'],
-//            ['title' => 'Աշակերտական խորհուրդ']
-//        ];
-//
-//        return $cat;
-//    }
-//}
+ <?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\DB;
+
+class CategoryFactory extends Factory
+{
+   /**
+    * Define the model's default state.
+    *
+    * @return array
+    */
+   public function definition()
+   {
+       Category::truncate();
+       $cat = [
+         ['title' => 'My Life'],
+            ['title' => 'My Family'],
+            ['title' => 'Friends'],
+       ];
+
+       return $cat;
+   }
+}
